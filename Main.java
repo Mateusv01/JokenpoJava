320217693181ebbc832c9b341f289bfef3f9e10e
Mateus <111800974+Mateusv01@users.noreply.github.com>
2023-08-27 12:08:25 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -11,26 +11,3 @@ public class Main {
 }
 
 
-//public void Jokenpo() {
-//	Random gerador = new Random();
-	
-//	int Selecao = gerador.nextInt();
-//	question.setText("");
-	
-//	switch (Selecao)
-//	{
-//	case 0:
-//	question.setText("O Computador escolheu: Pedra");
-//	break;
-//	case 1:
-//	question.setText("O Computador escolheu: Papel");
-//	break;
-//	case 2:
-//	question.setText("O Computador escolheu: Tesoura");
-//	break;
-//	}
-	
-//	question.setText("");
-//	if (Selecao == Selecao) {
-//			question.setText("Empate");
-//			}
